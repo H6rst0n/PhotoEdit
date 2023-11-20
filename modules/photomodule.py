f686abf1d58c936770037315a197fdc70ddea972
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2023-11-20 22:35:50 +0800

    1.1
    module and main execution files

---
+++ modules/photomodule.py
@@ -0,0 +1,71 @@
+# import zone
+from PIL import Image
+import os
+
+# Resolution compression.
+def Adjustment_Resolution(input_file, output_path, target_width):
+    # Open Original Photo
+    original_image = Image.open(input_file)
+
+    # Obtain the width and height of the original photo.
+    original_width, original_height = original_image.size
+
+    # Calculating the scaling ratio.
+    scale_factor = target_width / original_width
+    new_height = int(original_height * scale_factor)
+
+    # Resize photos.
+    resized_image = original_image.resize((target_width, new_height))
+
+    # Save the processed photo.
+    output_file = os.path.join(output_path, os.path.basename(input_file))
+    resized_image.save(output_file)
+
+
+# Vertical Photo Scaling.
+def Adjust_vertical_photo_ratio(input_file, output_path, color):
+    # Open Original Photo
+    original_image = Image.open(input_file)
+
+    # Check EXIF rotation information.
+    if hasattr(original_image, '_getexif'):
+        exif = original_image._getexif()
+        if exif is not None and 274 in exif:
+            orientation = exif[274]
+            # Processing based on rotation information.
+            if orientation == 3:
+                original_image = original_image.rotate(180, expand=True)
+            elif orientation == 6:
+                original_image = original_image.rotate(270, expand=True)
+            elif orientation == 8:
+                original_image = original_image.rotate(90, expand=True)
+
+    # Obtain the width and height of the original photo.
+    original_width, original_height = original_image.size
+
+    # Adjusts the resolution to the IG upper line and calculates the new width.
+    new_height = int(1350)
+    new_width = int((new_height / original_height) * original_width)
+    target_width = int(new_height * 0.8)
+
+    # Resize photos
+    resized_image = original_image.resize((new_width, new_height))
+
+    # Create a new canvas and fill it with the background colour (black or white).
+    if color == "w" or color == "W":
+        final_image = Image.new("RGB", (target_width, new_height), (255, 255, 255))
+    elif color == "b" or color == "B":
+        final_image = Image.new("RGB", (target_width, new_height), (0, 0, 0))
+    else:
+        print("Error!! Colour not supported. Only \"B\"lack or \"W\"hite.")
+        exit()
+
+    # Calculate the width of the left and right black borders.
+    border_width = (target_width - new_width) // 2
+
+    # Post the adjusted photo.
+    final_image.paste(resized_image, (border_width, 0))
+
+    # Save the processed photo.
+    output_file = os.path.join(output_path, os.path.basename(input_file))
+    final_image.save(output_file)
---
+++ photoeditor.py
@@ -0,0 +1,99 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+import modules.photomodule as pm
+
+selected_path = None
+output_path = None
+black_or_white_background = "B"
+
+def input_file_select():
+    global selected_path
+    selected_path = filedialog.askopenfilename(title="選擇輸入檔案", filetypes=[("JPEG files", "*.jpg"), ("All files", "*.*")])
+    input_show.config(text="輸入位置: " + selected_path)
+
+def input_directory_select():
+    global selected_path
+    selected_path = filedialog.askdirectory(title="選擇輸入目錄")
+    input_show.config(text="輸入位置: " + selected_path)    
+
+def output_directory_select():
+    global output_path
+    output_path = filedialog.askdirectory(title="選擇輸出目錄")    
+    output_show.config(text="輸入位置: " + output_path)
+    
+def resize_image():
+    if selected_path and os.path.isfile(selected_path):
+        pm.Adjustment_Resolution(selected_path, output_path, 1080)
+    elif selected_path and os.path.isdir(selected_path):
+        jpgfile = [file for file in os.listdir(selected_path) if file.endswith(".jpg")]
+        for file in jpgfile:
+            pm.Adjustment_Resolution(os.path.join(selected_path, file), output_path, 1080)
+
+def border_image():
+    if selected_path and os.path.isfile(selected_path):
+        pm.Adjust_vertical_photo_ratio(selected_path, output_path, "B")
+    elif selected_path and os.path.isdir(selected_path):
+        jpgfile = [file for file in os.listdir(selected_path) if file.endswith(".jpg")]
+        for file in jpgfile:
+            pm.Adjust_vertical_photo_ratio(os.path.join(selected_path, file), output_path, black_or_white_background)
+
+def black_or_white_background_select():
+    global black_or_white_background
+    if black_or_white_background == "B":
+        black_or_white_background = "W"
+        black_or_white_show.config(text="目前: 白")
+    elif black_or_white_background == "W":
+        black_or_white_background = "B"
+        black_or_white_show.config(text="目前: 黑")
+
+
+win = tk.Tk() # 建立主視窗
+
+# 標題
+win.title("photo editor")
+
+# 視窗調整
+win.geometry("800x500") # 解析度
+win.resizable(0,0) # 不可調整大小
+
+# Button
+input_file = tk.Button(win, text="單一檔案", command=input_file_select)
+input_file.config(font=("微軟正黑體", 14))
+input_file.place(x=10, y=10)
+
+input_directory = tk.Button(win, text="批量操作", command=input_directory_select)
+input_directory.config(font=("微軟正黑體", 14))
+input_directory.place(x=120, y=10)
+
+output_directory = tk.Button(win, text="輸出目錄位置", command=output_directory_select)
+output_directory.config(font=("微軟正黑體", 14))
+output_directory.place(x=10, y=90) 
+
+resize = tk.Button(win, text="解析度調整成IG上大小", bg="yellow", command=resize_image)
+resize.config(font=("微軟正黑體", 14))
+resize.place(x=10, y=190)
+
+Border = tk.Button(win, text="調整直幅照片成IG大小", bg="yellow", command=border_image)
+Border.config(font=("微軟正黑體", 14))
+Border.place(x=10, y=250)
+
+black_or_white = tk.Button(win, text="黑或白", command=black_or_white_background_select)
+black_or_white.config(font=("微軟正黑體", 14))
+black_or_white.place(x=230, y=250)
+
+# Lable
+input_show = tk.Label(win, text="輸入位置: ")
+input_show.config(font=("微軟正黑體", 12))
+input_show.place(x=10, y=60)
+
+output_show = tk.Label(win, text="輸出位置: ")
+output_show.config(font=("微軟正黑體", 12))
+output_show.place(x=10, y=140)
+
+black_or_white_show = tk.Label(win, text="目前: 黑")
+black_or_white_show.config(font=("微軟正黑體", 14))
+black_or_white_show.place(x=310, y=257)
+
+
+win.mainloop() # 常駐主視窗
